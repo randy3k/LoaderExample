f7f9aef0b28b92d8ff7ca60aec8aa5eee7449c63
Randy Lai <randy.cs.lai@gmail.com>
2017-08-06 18:55:48 -0400

    save and restore original modules

--- loader.py
+++ loader.py
@@ -5,19 +5,35 @@ from contextlib import contextmanager
 
 
 @contextmanager
-def intercepting_imports(modules):
-    finder = FilterFinder(modules)
+def intercepting_imports(module_names):
+    finder = FilterFinder(module_names)
     sys.meta_path.insert(0, finder)
+
+    # save the original modules to prevent overwritte modules of the same name
+    original_modules = {}
+    for m in module_names:
+        if m in sys.modules:
+            original_modules[m] = sys.modules[m]
+        else:
+            original_modules[m] = None
     try:
         yield
     finally:
         if finder in sys.meta_path:
             sys.meta_path.remove(finder)
 
+        # restore the original modules
+        for m in module_names:
+            if original_modules[m]:
+                sys.modules[m] = original_modules[m]
+            else:
+                if m in sys.modules:
+                    del sys.modules[m]
+
 
 class FilterFinder:
-    def __init__(self, modules):
-        self._modules = modules
+    def __init__(self, module_names):
+        self._modules = module_names
         self.deps_path = os.path.join(os.path.dirname(__file__), "deps")
 
     def find_module(self, name, path=None):
@@ -28,6 +44,7 @@ class FilterFinder:
             else:
                 return importlib.machinery.SourceFileLoader(name, path + ".py")
 
-modules = ["foo"]
-with intercepting_imports(modules):
+
+module_names = ["foo"]
+with intercepting_imports(module_names):
     from .src import *
