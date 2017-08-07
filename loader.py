d1edc012c02474de95279693bc95cdda3543a86d
Randy Lai <randy.cs.lai@gmail.com>
2017-08-07 15:17:03 -0400

    subclass finder from importlib.machinery.FileFinder

--- loader.py
+++ loader.py
@@ -4,9 +4,17 @@ import importlib
 from contextlib import contextmanager
 
 
+loader_details = [
+    (importlib.machinery.ExtensionFileLoader, importlib.machinery.EXTENSION_SUFFIXES),
+    (importlib.machinery.SourceFileLoader, importlib.machinery.SOURCE_SUFFIXES),
+    (importlib.machinery.SourcelessFileLoader, importlib.machinery.BYTECODE_SUFFIXES)
+]
+
+
 @contextmanager
 def intercepting_imports(module_names):
-    finder = FilterFinder(module_names)
+    deps_path = os.path.join(os.path.dirname(__file__), "deps")
+    finder = FilterFileFinder(module_names, deps_path, *loader_details)
     sys.meta_path.insert(0, finder)
 
     # save the original modules to prevent overwritte modules of the same name
@@ -31,18 +39,14 @@ def intercepting_imports(module_names):
                     del sys.modules[m]
 
 
-class FilterFinder:
-    def __init__(self, module_names):
-        self._modules = module_names
-        self.deps_path = os.path.join(os.path.dirname(__file__), "deps")
+class FilterFileFinder(importlib.machinery.FileFinder):
+    def __init__(self, module_names, path, *loader_details):
+        self._module_names = module_names
+        super().__init__(path, *loader_details)
 
     def find_module(self, name, path=None):
-        if name in self._modules:
-            path = os.path.join(self.deps_path, name)
-            if os.path.isdir(path):
-                return importlib.machinery.SourceFileLoader(name, os.path.join(path, "__init__.py"))
-            else:
-                return importlib.machinery.SourceFileLoader(name, path + ".py")
+        if name in self._module_names:
+            return super().find_module(name)
 
 
 module_names = ["foo"]
